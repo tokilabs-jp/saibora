529fdc56647359d06ef397742e191bc819358924
hirokazutoki <hrkztk1124@gmail.com>
2026-05-14 22:46:03 +0900

    update: modify database seeder and user factory default values

--- saibora/database/factories/UserFactory.php
+++ saibora/database/factories/UserFactory.php
@@ -30,7 +30,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('Pass1234@'),
             'remember_token' => Str::random(10),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
