6dd09c7333308f807fa278f47fce5f3d7e043d82
hirokazutoki <hrkztk1124@gmail.com>
2026-05-20 13:52:02 +0900

    add: implement search filter in admin users index using Spatie Query Builder

--- saibora/app/Http/Controllers/Admin/UserController.php
+++ saibora/app/Http/Controllers/Admin/UserController.php
@@ -5,12 +5,25 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $users = User::orderByDesc('id')->paginate();
+        $users = QueryBuilder::for(User::class)
+            ->allowedFilters(
+                // Global search across id and email
+                AllowedFilter::callback('search', function ($query, $value) {
+                    $query->where(function ($query) use ($value) {
+                        $query->where('id', "{$value}")
+                            ->orWhere('email', "{$value}");
+                    });
+                })
+            )
+            ->orderByDesc('id')
+            ->paginate();
 
         return view('admin.users.index', compact('users'));
     }
