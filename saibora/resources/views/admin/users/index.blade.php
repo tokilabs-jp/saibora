97c80267e95a56328903ffe516971da0362e8d2b
hirokazutoki <hrkztk1124@gmail.com>
2026-05-22 23:20:03 +0900

    add: create user modal with Flux component integration in admin user index

--- saibora/resources/views/admin/users/index.blade.php
+++ saibora/resources/views/admin/users/index.blade.php
@@ -1,9 +1,15 @@
-<x-layouts::admin :title="__('Users')">
+<x-layouts::admin :title="__('Users')" xmlns:flux="http://www.w3.org/1999/html">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="flex items-center justify-between">
             <h1 class="text-2xl inline">Users</h1>
-            @include('admin.users._search-form')
+            <span class="flex justify-end">
+                <flux:modal.trigger name="create-user">
+                    <flux:button variant="primary">Create user</flux:button>
+                </flux:modal.trigger>
+            </span>
         </div>
+        @include('admin.users._search-form')
         @include('admin.users._list')
     </div>
+    @livewire('admin.users.create-user')
 </x-layouts::admin>
---
+++ saibora/resources/views/components/admin/users/⚡create-user.blade.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Models\User;
+use Flux\Flux;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+use Livewire\Component;
+
+new class extends Component {
+    public string $name = '';
+
+    public string $email = '';
+
+    public string $password = '';
+
+    public function save(): void
+    {
+        $validated = $this->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+        ]);
+
+        $validated['password'] = Hash::make(str()->random(16));
+
+        User::create($validated);
+
+        $this->reset(['name', 'email', 'password']);
+
+        Flux::modal('create-user')->close();
+
+        Flux::toast(
+            text: 'User has been created.',
+            heading: 'User created',
+            variant: 'success',
+        );
+    }
+};
+?>
+
+<flux:modal name="create-user" flyout variant="floating" class="md:w-lg" :dismissible="false">
+    <form wire:submit="save" class="space-y-6">
+        <flux:heading size="lg">Create user</flux:heading>
+
+        <flux:input label="Email" name="email" type="email" wire:model="email" />
+
+        <flux:input label="Name" name="name" wire:model="name" />
+
+        <div class="flex">
+            <flux:spacer />
+
+            <flux:button type="submit" variant="primary">
+                Save
+            </flux:button>
+        </div>
+    </form>
+</flux:modal>
